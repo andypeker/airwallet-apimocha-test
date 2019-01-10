fdaaab73b8fa86d7cb51c3f25b7c7ea0b2294f46
franklin <franklin@carolAir.yang.net>
2019-01-11 01:01:13 +0800

    modified:   apitest-mocha.js

--- apitest-mocha.js
+++ apitest-mocha.js
@@ -1,3 +1,10 @@
 
 
+
 //	https://blog.csdn.net/m0_37263637/article/details/79731825
+//	https://www.jianshu.com/p/47575895bc54
+
+//	https://www.cnblogs.com/Leo_wl/p/5734889.html
+
+
+//	http://www.ruanyifeng.com/blog/2015/12/a-mocha-tutorial-of-examples.html
